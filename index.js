9e89461702143c5f5fcbf9e29d3890ede15b7538
Nadim Absi <absinad1@gmail.com>
2022-10-19 09:28:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun())
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(effect="*"){
+    return ((adjective="special") => `You are ${effect}${adjective}${effect}!`)
+}
+console.log(wrapAdjective("99")("superduper"))
 // code your solution here
